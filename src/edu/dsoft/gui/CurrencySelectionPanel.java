b710b478fde36bfb37206d9c8850f3e0a9e7ba68
G.L <g.l@gmail.com>
2022-12-13 14:00:05 +0100

    - Added CurrencySelectionPanel Form

---
+++ src/edu/dsoft/gui/CurrencySelectionPanel.java
@@ -0,0 +1,15 @@
+package edu.dsoft.gui;
+
+import javax.swing.*;
+
+public class CurrencySelectionPanel {
+    private JComboBox comboBox1;
+    private JPanel panel1;
+    private JComboBox comboBox2;
+    private JButton pobierzDaneButton;
+    private JTextField PLNEURTextField;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
